798fb1cb9084645ed449af82990ecad687cafdf4
Md. Shirajul Islam <shirajraj@gmail.com>
2023-06-21 23:55:10 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const submit = document.getElementById('submit')
+submit.addEventListener('click', function(){
+    const inputBtn = document.getElementById('inputCity').value;
+    fetch(`https://api.weatherapi.com/v1/current.json?key=6cf483cc3a4f4b499ff174759232106 &q=${inputBtn}&aqi=no`)
+    .then(response => response.json())
+    .then(data => {
+        const cityName = data.location.name;
+        const temp = data.current.temp_c;
+        const description = data.current.condition.text;
+        const icon = data.current.condition.icon;
+        const county = data.location.country;
+        const hum = data.current.humidity;
+        
+        document.getElementById('city').innerHTML = "City Name: "+ cityName;
+        document.getElementById('temp').innerHTML = "Temperature: "+ temp+"&deg;C";
+        document.getElementById('des').innerHTML = "Weather Condition: "+ description;
+        document.getElementById('country').innerHTML = "Country: "+county;
+        // document.getElementById('icon').innerHTML = icon;
+        document.getElementById('hum').innerHTML = "Current Humidity: "+hum+"%";
+    })
+    .catch(res => alert("Please Enter Right City Name"))
+})
